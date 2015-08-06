70f9530a671a4b074c998459b9ab40d649e6e015
carlos <carlos@loyal3.com>
2015-08-06 08:36:35 -0700

    Web server & Led (i2c) Hello World

---
+++ 03_web_gpio_led.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# LED Blinky code for Raspberry Pi on flask webserver
+# Written in Python 2.7.3 by Carlos Vega
+
+from flask import Flask
+import RPi.GPIO as GPIO     ## Import GPIO Library
+import time                 ## Import 'time' library (for 'sleep')
+
+app = Flask(__name__)
+
+## Define function named ledBlink()
+def ledBlink(pin, delay):
+    while True:
+        GPIO.output(pin, GPIO.HIGH)     ## Turn on GPIO pin (HIGH)
+        time.sleep(delay)               ## Wait
+        GPIO.output(pin, GPIO.LOW)      ## Turn off GPIO pin (LOW)
+        time.sleep(delay)               ## Wait
+
+
+@app.route("/") # take note of this decorator syntax, it's a common pattern
+def hello():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.OUT)
+    ledBlink(7, 1)
+    GPIO.cleanup()
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
